3d148b53927af8e6a94632a608eb0b3b53344caa
Daniel <mwdchang@gmail.com>
2022-07-06 00:51:44 -0400

    tree-like search and layout for threads

--- ui/search.js
+++ ui/search.js
@@ -171,7 +171,7 @@ export const betterSearch3 = (searchStr, corpus) => {
   console.log(indirectMatches.map(d => d.text));
 
   return [directMatches, indirectMatches, Object.keys(temp)];
-}
+};
 
 export const distance = (v1, v2) => {
   let total = 0;
@@ -179,6 +179,132 @@ export const distance = (v1, v2) => {
     total += (v1[i] - v2[i]) * (v1[i] - v2[i]);
   }
   return Math.sqrt(total);
-}
+};
+
+
+/**
+ * Given
+ * [
+ *   {
+ *     person: [tokenX, tokenY]
+ *     org: [...]
+ *   },
+ *   {
+ *     org: [...],
+ *     ...
+ *   }
+ * ]     
+ *
+ * Transform to 
+ *   [tokenA, tokenB ...]
+ */
+const extractTopNER = (ners) => {
+  // Find sentence level NER
+  const totalNERMap = new Map();
+  for (const ner of ners) {
+    for (const [_k, v] of Object.entries(ner)) {
+      for (const token of v) {
+        if (totalNERMap.has(token)) {
+          totalNERMap.set(token, totalNERMap.get(token) + 1);
+        } else {
+          totalNERMap.set(token, 1);
+        }
+      }
+    }
+  }
+
+  const topNER = [...totalNERMap.entries()].sort((a, b) => b[1] - a[1]).splice(0, 20);
+  return topNER.map(d => d[0]);
+};
+
+
+
+export const betterSearch4 = (searchStr, corpus) => {
+  // 1. Direct matching
+  const directMatches = corpus.filter(doc => {
+    let found = false;
+    doc.sents.forEach(sent => {
+      if (sent.text.toLowerCase().includes(searchStr)) {
+        found = true;
+      }
+    });
+    return found;
+  });
+
+
+  console.log('');
+  console.log('======== Better search 4 ========');
+  console.log('# total docs', corpus.length);
+  console.log('# direct matches', directMatches.length);
+
+
+  // 2. For each direct match, find similar documents
+  const dupeMap = new Map();
+  for (const doc of directMatches) {
+    dupeMap.set(doc.id, 1);
+  }
+  const availableDocs = corpus.filter(d => ! dupeMap.has(d.id));
+
+  console.log('# remaining for secondary search', availableDocs.length);
+
+  const level2Map = {};
+
+  for (const doc of directMatches) {
+    const nounChunks = [];
+    const ners = [];
+
+    level2Map[doc.id] = [];
+
+    // Extract the language properties for the matched doc
+    doc.sents.forEach(sent => {
+      if (sent.text.toLowerCase().includes(searchStr)) {
+        const sNER = sent.ner;
+        delete sNER.CARDINAL;
+        delete sNER.ORDINAL;
+        delete sNER.DATE;
+        const testChunks = sent.noun_chunks.filter(d => d.split(' ').length > 1);
+        testChunks.forEach(d => {
+          nounChunks.push(d);
+        });
+
+        if (Object.keys(sNER).length > 0) {
+          ners.push(sent.ner);
+        }
+      }
+    });
+
+    const topNERs = extractTopNER(ners);
+
+    // Search through remaining available for similarity
+    for (const candidateDoc of availableDocs) {
+      let cnt = 0;
+      const matchedTokens = [];
+      for (const token of topNERs) {
+        if (candidateDoc.text.includes(token)) {
+          cnt++;
+          matchedTokens.push(token);
+        }
+      }
+
+      for (const chunk of nounChunks) {
+        if (candidateDoc.text.includes(chunk)) {
+          cnt++;
+          matchedTokens.push(chunk);
+        }
+      }
+
+      if (cnt > 2) {
+        console.log('\tFound secondary', doc.id);
+
+        level2Map[doc.id].push({
+          doc: candidateDoc,
+          matchedTokens: matchedTokens
+        });
+      }
+    }
+  }
+
+  return [directMatches, level2Map];
+};
 
 // https://www.kaggle.com/code/thebrownviking20/topic-modelling-with-spacy-and-scikit-learn/notebook
